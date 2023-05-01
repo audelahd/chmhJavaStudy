08d89d3cdca90b287cb93889a17e57b0153500cc
myeong <audelahd@gmail.com>
2023-05-01 16:17:14 +0900

    Exception 예제, 제네릭 타입 예제, 사용자 정의 예제

---
+++ ch13/sec02/exam02/HomeAgency.java
@@ -0,0 +1,8 @@
+package ch13.sec02.exam02;
+
+public class HomeAgency implements Rentable<Home>{
+
+	public Home rent() {
+		return new Home();
+	}
+}
---
+++ test/day28/Jinsu.java
@@ -0,0 +1,18 @@
+package test.day28;
+
+public class Jinsu {
+
+	public static void main(String[] args) {
+		
+		
+		
+		int ken =50;
+
+		System.out.println("10진수는 :"+ken);
+		System.out.println("2진수는 :"+Integer.toBinaryString(ken));
+		System.out.println("8진수는 :"+Integer.toOctalString(ken));
+		System.out.println("16진수는 :" +Integer.toHexString(ken));
+
+	}
+
+}
